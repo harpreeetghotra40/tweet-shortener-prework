2619ab9bc05efef0e83fad53edfae1399e73e0c9
HARPREET GHOTRA <harpreetghotra40@icloud.com>
2019-05-15 23:08:31 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,34 @@
 # Write your code here.
+def dictionary
+  words = {
+    "hello" => "hi",
+    "to" => '2',
+    "two" => '2',
+    "too" => '2',
+    "for" => '4',
+    "For" => '4',
+    "four" => '4',
+    "be" => 'b',
+    "you" => 'u',
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter(tweet)
+  wordsArray = tweet.split(" ")
+  wordsArray.each_with_index do |words , i|
+    dictionary.keys.each do |x|
+      if x == words
+      wordsArray[i] = dictionary[x]
+      end
+    end
+  end
+wordsArray.join(" ")
+end
+
+def bulk_tweet_shortener(tweets)
+  tweets.each do |tweet|
+    puts word_substituter(tweet)
+  end
+end
