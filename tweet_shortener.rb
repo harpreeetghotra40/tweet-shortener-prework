2c30d27340e8cdf7a8a4b737e7126fce32d7ee40
HARPREET GHOTRA <harpreetghotra40@icloud.com>
2019-05-15 23:21:33 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -33,10 +33,10 @@ def bulk_tweet_shortener(tweets)
   end
 end
 
-def selective_tweet_shortener(tweets)
-  tweets.each do |tweet|
-    if tweet.length > 140
-      puts word_substituter(tweet)
-    
-    end
+def selective_tweet_shortener (tweet)
+if tweet.length > 140
+  word_substituter(tweet)
+else
+ tweet
+end
 end
